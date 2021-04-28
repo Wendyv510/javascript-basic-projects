cbc47ffc5a4206170ccae6ea4539a9739e9f4ab8
Wendy Harris <wendyv510@hotmail.com>
2021-04-28 11:26:21 -0400

    conter index

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -6,6 +6,12 @@ const color = document.querySelector(".color");
 btn.addEventListener('click', function() {
 let hexColor = '#'; 
 for(let i = 0; i < 6; i++){
-    hexColor
+    hexColor += hex[getRandomNumber()]
 }
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor 
+})
+
+function getRandomNumber(0 {
+    return Math.floor(Math.random() * hex.length)
 })
